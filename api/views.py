15d2a0be4e472f45d952433058b4e4876046262a
Denis Ngugi Gathondu <denis.gathondu@yahoo.com>
2020-04-15 02:55:04 +0300

    fix views
    Signed-off-by: Denis Ngugi Gathondu <denis.gathondu@yahoo.com>

--- api/views.py
+++ api/views.py
@@ -10,14 +10,14 @@ from src.estimator import estimator
 
 @api_view(['POST',])
 def index(request):
-    data = parse_request(request.data)
+    data = json.loads(request.body)
     return Response(estimator(data))
 
 
 @api_view(['POST',])
 @renderer_classes((XMLRenderer,))
 def xml(request):
-    data = parse_request(request.data)
+    data = json.loads(request.body)
     return Response(estimator(data))
 
 
@@ -25,17 +25,7 @@ def xml(request):
 def logs(request):
     DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     data = []
-    with open(os.path.join(DIR, 'requests.txt'), 'r') as reader:
+    with open(os.path.join(DIR, 'log.txt'), 'r') as reader:
         for line in reader.readlines():
             data.append(line.replace('\n', ''))
     return Response(data, content_type='text/plain; charset=UTF-8')
-
-
-def parse_request(request):
-    data = dict()
-    for key in request.keys():
-        if key == 'region':
-            data.setdefault(key, json.loads(request.get(key)))
-        else:
-            data.setdefault(key, request.get(key))
-    return data
